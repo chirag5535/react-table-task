d38989eecd661015a789b6b2bb8a421e50dffe4c
Chirga sheta <crsheta5535@gmail.com>
2024-11-25 10:59:27 +0530

    add build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler", // or "modern"
-      },
-    },
-  },
+  base: '/react-table-task/', // Replace with your repository name
 });
