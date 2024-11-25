5b6bee10f42d84e36c09b2c77e20f403e3ec764e
Chirga sheta <crsheta5535@gmail.com>
2024-11-25 11:12:38 +0530

    add env file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-table-task/', // Replace with your repository name
+  base: process.env.VITE_BASE_URL || '/',  // Use base URL from .env
 });
